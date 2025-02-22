148ccde785f03336e1a31c91a9f6ff2a5e6552d2
oahlman <oskar.ahlman@peerdigital.se>
2025-02-22 21:39:57 +0100

    Revert "feat: add APIKey and UsageRecord models"
    This reverts commit 9690b97be157e7abde61acecc0b4a5837431f319.

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -3,13 +3,9 @@
 from .base import Base
 from .user import User
 from .system_settings import SystemSettings
-from .api_key import APIKey
-from .usage_record import UsageRecord
 
 __all__ = [
     "Base",
     "User",
     "SystemSettings",
-    "APIKey",
-    "UsageRecord",
 ]
