07986f808e1609cd92f3a4c3b1ae51011cfcdc52
PeerDigitalAdam <80524136+PeerDigitalAdam@users.noreply.github.com>
2025-03-08 20:04:14 +0100

    field update

--- frontend/admin-dashboard/src/pages/Users.tsx
+++ frontend/admin-dashboard/src/pages/Users.tsx
@@ -225,6 +225,7 @@ const Users: React.FC = () => {
                 <TableCell>Name</TableCell>
                 <TableCell>Email</TableCell>
                 <TableCell>Role</TableCell>
+                <TableCell>Status</TableCell>
                 <TableCell>Email Verified</TableCell>
                 <TableCell>Token Limit</TableCell>
                 <TableCell>Referral Code</TableCell>
